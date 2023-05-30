0bcbefdb3836b2bc86ca98e05b1bb137296c833a
jenry <Jenrycn02@outlook.com>
2023-05-29 21:29:59 -0300

    criptografia BCryptPasswordEncoder

--- src/main/java/com/jenry/curso/security/config/SecurityConfig.java
+++ src/main/java/com/jenry/curso/security/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.jenry.curso.security.config;
 
+import com.jenry.curso.security.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,4 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutSuccessUrl("/");
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    //passwordEncoder= tipo de crictografia que desejar
 }
