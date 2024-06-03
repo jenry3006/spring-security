022d90856bc07dd9d4869751a2d22287af44ca99
jenry <Jenrycn02@outlook.com>
2024-06-03 10:04:06 -0300

    removendo agendamentos

--- src/main/java/com/jenry/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/jenry/curso/security/web/controller/AgendamentoController.java
@@ -102,8 +102,11 @@ public class AgendamentoController {
         return "redirect:/agendamentos/agendar";
     }
 
+    @GetMapping("/excluir/consulta/{id}")
+    public String excluirConsulta(@PathVariable("id") Long id, RedirectAttributes attr){
+        service.remover(id);
+        attr.addFlashAttribute("sucesso","Consulta excluida com sucesso!");
+        return "redirect:/agendamentos/historico/paciente";
+    }
+
 }
-
-
-
-
